7268c3784ff18fab1b02d74191bbc27854fa14ff
vamshikrishna12345678 <chagantivamshikrishna1995@gmail.com>
2024-04-03 18:13:54 +0530

    add a new file

--- src/jdbc_project1/Hello.java
+++ src/jdbc_project1/Hello.java
@@ -3,6 +3,7 @@ package jdbc_project1;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
+	    System.out.println("hi java");
 	}
 
 }
---
+++ src/jdbc_project1/Welcome.java
@@ -0,0 +1,9 @@
+package jdbc_project1;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
